afb6b6e3e64bd79e720803a05cb5999de1c88338
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-04 20:20:11 +0600

    Solved Free Drinksproblem

--- 1 Free Drinks.js
+++ 1 Free Drinks.js
@@ -4,3 +4,11 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+
+let burger = 550;
+if (burger >= 500) {
+    console.log("Congratulation!You got free coke")
+}
+else {
+    console.log("Please,pay 30tk for coke")
+}
