3350bd702d3715697708ad0f8cc31e932f5dad1b
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-04 22:10:43 +0600

    solved nested friends problem

--- 4 nested-friend.js
+++ 4 nested-friend.js
@@ -10,3 +10,23 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+let yourNum = 86;
+let friendNum = 35;
+
+if (yourNum > 80) {
+    if (friendNum > 80) {
+        console.log("then go for a lunch.");
+    }
+    else if (friendNum < 80 && friendNum >= 60) {
+        console.log("then tell your friend, good luck next time.");
+    }
+    else if (friendNum < 60 && friendNum >= 40) {
+        console.log("keep your friend's message unseen.");
+    }
+    else {
+        console.log("block your friend");
+    }
+}
+else {
+    console.log("go to home and sleep and act sad");
+}
